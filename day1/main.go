17d382d2d9308360eaf632c1c54355124531d415
Richard Johansson <richard.johansson.8003@gmail.com>
2022-12-05 16:43:02 +0100

    foobar

--- day1/main.go
+++ day1/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -13,46 +12,30 @@ import (
 var input string
 
 func main() {
-	data := creataArryOfValues()
 
-	fmt.Printf("Task 1: %v\n", data[0])
-	fmt.Printf("Task 2: %v\n", data[0]+data[1]+data[2])
+	fmt.Printf("Task 1: %v\n", foobar()[0])
+	fmt.Printf("Task 2: %v\n", foobar()[0]+foobar()[1]+foobar()[2])
 
 }
 
-func creataArryOfValues() []int {
+func foobar() []int {
 	var data []int
 
-	strData := strings.Split(input, "\n\n")
+	for _, group := range strings.Split(input, "\n\n") {
 
-	for _, set := range strData {
-		sumOfSet := getSumOfSet(set)
-		data = append(data, sumOfSet)
+		test := strings.Split(string(group), "\n")
+		var groupSum int
+		for _, t := range test {
+			num, _ := strconv.Atoi(t)
+			groupSum += num
+		}
+		data = append(data, groupSum)
 	}
+
 	sort.Slice(data, func(i, j int) bool {
 		return data[i] > data[j]
 	})
 
 	return data
-}
 
-// used by creataArryOfValues
-func getSumOfSet(set string) int {
-	var sum int
-
-	slicedSet := strings.Split(string(set), "\n")
-	for _, el := range slicedSet {
-
-		if el != "" {
-
-			elInt, err := strconv.Atoi(el)
-			if err != nil {
-				fmt.Println(err.Error())
-				os.Exit(1)
-			}
-			sum += elInt
-		}
-	}
-
-	return sum
 }
